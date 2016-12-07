d91a0177e5c3a19a69c672a36b2844d6d3f631dc
opnmind <opnmind@mailbox.org>
2016-12-07 10:31:28 +0100

    Update SVGBezierApproximator.php

--- src/Rasterization/Path/SVGBezierApproximator.php
+++ src/Rasterization/Path/SVGBezierApproximator.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace JangoBrick\SVG\Rasterization\Path;
+namespace Opnmind\SVG\Rasterization\Path;
 
 /**
  * This class can approximate quadratic and cubic Bézier curves by calculating
